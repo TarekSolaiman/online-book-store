3d25359012cff184b84ad4672ac31eefec05d07f
MORSHED-HOME <morshed952640@gmail.com>
2023-01-12 01:03:34 +0600

    Why BookShip section structure are ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import WhyBookShip from "./components/WhyBookShip/WhyBookShip";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold">Online-book-store</h1>
+      <WhyBookShip></WhyBookShip>
     </div>
   );
 }
