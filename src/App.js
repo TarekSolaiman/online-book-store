14a46b4f29dd2188cdb5657451836ac1ee6b8a51
Tarek <mdtareksolaiman16@gmail.com>
2023-01-11 12:08:25 +0600

    Add footer in appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold">Online-book-store</h1>
+      <Navbar />
     </div>
   );
 }
